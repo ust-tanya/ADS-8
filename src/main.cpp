792f92e5ea794fcbc647982ffc8513300def784d
ust-tanya <94698360+ust-tanya@users.noreply.github.com>
2022-06-10 21:15:38 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,15 @@
-// Copyright 2021 NNTU-CS
+// Copyright 2022 NNTU-CS
+#include <iostream>
 #include "train.h"
 
-Train::Cage *Train::create(bool light) {
-  Cage* carrg = new Cage;
-  carrg->light = light;
-  carrg->next = carrg->prev = nullptr;
-  return carrg;
-}
+int main() {
+  Train train;
+  int count = 60; // кол-во вагонов
 
-Train::Train() {
-  first = current = nullptr;
-  countOp = len = Count = 0;
-}
+  while (count--)
+    train.addCage(false);
 
-void Train::addCage(bool light) {
-  if (!(first)) {
-  first = create(light);
-  current = first;
-  } else {
-  current->next = create(light);
-  current->next->prev = current;
-  current = current->next;
-  if (!current->next) {
-  current->next = first;
-  } else {
-  first->prev = current;
-  }
-  }
-}
-
-int Train::getLength() {
-  first->light = true;
-  current = first;
-  int num;
-  while (true) {
-  ++countOp, ++Count;
-  current = current->next;
-  if (current->light) {
-  num = Count;
-  current->light = false;
-  if ((current->prev) != nullptr) {
-  while (current->light == false) {
-  current = current->prev;
-  --Count, ++countOp;
-  }
-  }
-  if (!current->light) {
-  len = num;
-  break;
-  }
-  }
-  }
-  countOp += len;
-  return len;
-}
-
-int Train::getOpCount() {
-  return countOp;
+  std::cout << train.getLength() << std::endl;
+  std::cout << train.getOpCount() << std::endl;
+  return 0;
 }
